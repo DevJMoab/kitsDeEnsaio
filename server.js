85b67e93aff411af06229badec9bd00a5fe459d1
Johan Moab <engjmoab@gmail.com>
2020-05-23 16:25:31 -0300

    Criamos as pastas view e public, instalamos nunjucks, mudamos as rotas e preparamos o projeto para trabalharmos com Template Engine

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express') 
+const nunjucks = require('nunjucks')
 
-const server = express() 
+const server = express()
 
-server.get("/", function(req, res){     
-    return res.send("Testando Servidor... ")
-})                                      
+server.set("view engine", "html")
 
+server.use(express.static('public'))
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get("/", function(req, res){       
+    return res.render("index")            
+})
+
+server.get("/kits", function(req, res){       
+    return res.render("kits", ) 
+})
 
 server.listen(5000, function(){         
     console.log("Server is Running!");  
