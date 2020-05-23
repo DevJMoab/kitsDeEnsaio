b20bce66d3d1bfa4bfa2fb868b483f4bdb043c1f
Johan Moab <engjmoab@gmail.com>
2020-05-23 16:45:46 -0300

    Consfigurado o Template Engine e automatizador de dados dinamicos

---
+++ data.js
@@ -0,0 +1,45 @@
+module.exports = [
+    {
+        id: "jMCLv4bHRhs",
+        img: "https://img.youtube.com/vi/jMCLv4bHRhs/maxresdefault.jpg",
+        title: "Resgatado",
+        duration: "5:02",
+        price: "Grátis"
+    },
+    {
+        id: "F18qxXKJ1Dg",
+        img: "https://img.youtube.com/vi/F18qxXKJ1Dg/maxresdefault.jpg",
+        title: "Resgatado - PlayBack",
+        duration: "5:01",
+        price: "Grátis"
+    },
+    {
+        id: "UrFiPTHIYU4",
+        img: "https://img.youtube.com/vi/UrFiPTHIYU4/maxresdefault.jpg",
+        title: "Kit Resgatado - 1 Tenor",
+        duration: "5:10",
+        price: "Grátis"
+    },
+    {
+        id: "N31upEPGfwo",
+        img: "https://img.youtube.com/vi/N31upEPGfwo/maxresdefault.jpg",
+        title: "Kit Resgatado - 2 Tenor",
+        duration: "5:01",
+        price: "Grátis"
+    },
+    {
+        id: "Ud9X7iUxj74",
+        img: "https://img.youtube.com/vi/Ud9X7iUxj74/maxresdefault.jpg",
+        title: "Kit Resgatado - Baritono",
+        duration: "5:19",
+        price: "Grátis"
+    },
+    {
+        id: "Ha3TQ9cB9",
+        img: "https://img.youtube.com/vi/Ha3TQ9cB9-o/maxresdefault.jpg",
+        title: "Kit Resgatado - Baixo",
+        duration: "5:14",
+        price: "Grátis"
+    }
+    
+]
