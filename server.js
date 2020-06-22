d01e5ba4b577e6b68e4fdf2624970a2d1f5b2bd0
Johan Moab <engjmoab@gmail.com>
2020-06-22 20:36:32 -0300

    remove cards priece

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express') 
+const server = express() 
+const nunjucks = require('nunjucks') 
+const videos = require("./data")               
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false,
+    noCache: true
+})
+
+server.set("view engine", "njk")
+
+server.use(express.static('public')) 
+
+server.get("/", function(req, res){
+    const dataIndex = {
+        avatar_url: "https://gravadora.novotempo.com/artistas/artista_11.jpeg",
+        name: "Arautos ",
+        surname: "do Rei",
+        description: "Este material está sendo disponível para que você possa ensaiar com seu quarteto. Apreveite! Deus os Abençoe.",
+        links: [
+            { name:"YouTube", url:"https://www.youtube.com/channel/UCldCtvi4dAqZvd5-QLf5QvQ"},
+            { name:"Facebook", url:"https://www.facebook.com/arautosdoreioficial"},
+            { name:"Instagram", url:"https://www.instagram.com/arautosdoreioficial/"}
+        ]
+    }
+    return res.render("index", {dataIndex})            
+})
+
+ 
+server.get("/kits", function(req, res){         
+    return res.render("kits", {items: videos})  
+})
+
+server.get("/video", function(req, res){
+    const id = req.query.id
+
+    const video = videos.find(function(video){
+        return video.id == id
+    })
+
+       if(!video){
+        return res.render("404", )}
+
+    return res.render("video", { item: video })
+})
+
+server.listen(5500, function(){         
+    console.log("Server is Running!");  
+})                                       
